e90d7fcc74848c4832af78f61d8bb7d428da4655
Mohamed Amir <m.elamir027@gmail.com>
2026-05-08 19:30:07 +0000

    chore: edit the db url

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,7 +1,8 @@
 import { Pool } from 'pg'
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString:
+    process.env.shopify_instashop_sync_DATABASE_URL,
   ssl: { rejectUnauthorized: false },
   max: 5,
   idleTimeoutMillis: 30_000,
